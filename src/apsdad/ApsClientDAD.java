d6ca03ad6ba0abe87c8561722f5df699778d05c9
Thiago <thiagolc3@gmail.com>
2016-10-19 17:30:55 -0200

    remoção de lixo

--- src/apsdad/ApsClientDAD.java
+++ src/apsdad/ApsClientDAD.java
@@ -11,8 +11,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
 
 /**
  *
